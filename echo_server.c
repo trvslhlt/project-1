f062252de74f62b73a478a489c984a5392a6cafc
travis <trvslhlt@gmail.com>
2017-02-02 17:17:21 +0000

    reformat server code

--- echo_server.c
+++ echo_server.c
@@ -22,18 +22,15 @@
 #define ECHO_PORT 9999
 #define BUF_SIZE 4096
 
-int close_socket(int sock)
-{
-    if (close(sock))
-    {
+int close_socket(int sock) {
+    if (close(sock)) {
         fprintf(stderr, "Failed closing socket.\n");
         return 1;
     }
     return 0;
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     int sock, client_sock;
     ssize_t readret;
     socklen_t cli_size;
@@ -43,8 +40,7 @@ int main(int argc, char* argv[])
     fprintf(stdout, "----- Echo Server -----\n");
     
     /* all networked programs must create a socket */
-    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
-    {
+    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
         fprintf(stderr, "Failed creating socket.\n");
         return EXIT_FAILURE;
     }
@@ -54,28 +50,23 @@ int main(int argc, char* argv[])
     addr.sin_addr.s_addr = INADDR_ANY;
 
     /* servers bind sockets to ports---notify the OS they accept connections */
-    if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)))
-    {
+    if (bind(sock, (struct sockaddr *) &addr, sizeof(addr))) {
         close_socket(sock);
         fprintf(stderr, "Failed binding socket.\n");
         return EXIT_FAILURE;
     }
 
 
-    if (listen(sock, 5))
-    {
+    if (listen(sock, 5)) {
         close_socket(sock);
         fprintf(stderr, "Error listening on socket.\n");
         return EXIT_FAILURE;
     }
 
     /* finally, loop waiting for input and then write it back */
-    while (1)
-    {
+    while (1) {
        cli_size = sizeof(cli_addr);
-       if ((client_sock = accept(sock, (struct sockaddr *) &cli_addr,
-                                 &cli_size)) == -1)
-       {
+       if ((client_sock = accept(sock, (struct sockaddr *) &cli_addr, &cli_size)) == -1) {
            close(sock);
            fprintf(stderr, "Error accepting connection.\n");
            return EXIT_FAILURE;
@@ -83,10 +74,8 @@ int main(int argc, char* argv[])
        
        readret = 0;
 
-       while((readret = recv(client_sock, buf, BUF_SIZE, 0)) >= 1)
-       {
-           if (send(client_sock, buf, readret, 0) != readret)
-           {
+       while((readret = recv(client_sock, buf, BUF_SIZE, 0)) >= 1) {
+           if (send(client_sock, buf, readret, 0) != readret) {
                close_socket(client_sock);
                close_socket(sock);
                fprintf(stderr, "Error sending to client.\n");
@@ -95,16 +84,14 @@ int main(int argc, char* argv[])
            memset(buf, 0, BUF_SIZE);
        } 
 
-       if (readret == -1)
-       {
+       if (readret == -1) {
            close_socket(client_sock);
            close_socket(sock);
            fprintf(stderr, "Error reading from client socket.\n");
            return EXIT_FAILURE;
        }
 
-       if (close_socket(client_sock))
-       {
+       if (close_socket(client_sock)) {
            close_socket(sock);
            fprintf(stderr, "Error closing client socket.\n");
            return EXIT_FAILURE;
