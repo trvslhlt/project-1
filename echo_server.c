6dd4da69a4bac5378ac9febc11749fb2e60685e2
trvslhlt <trvslhlt@gmail.com>
2017-02-25 14:05:34 -0500

    handling multiple clients

--- echo_server.c
+++ echo_server.c
@@ -25,61 +25,69 @@
 int close_socket(int);
 
 int main(int argc, char* argv[]) {
-    int server_sock, min_sock, max_sock, new_sock;
-    // select changes the fd_set.
-    // we need master to store info and read_set to indicate socketsready to read
-    fd_set master_set, read_set;
-    socklen_t cli_size;
-    struct sockaddr_in addr, cli_addr;
-    char buf[BUF_SIZE];
-    int i;
-    ssize_t nbytes;
+  int server_sock, min_sock, max_sock, new_sock;
+  // select changes the fd_set.
+  // we need master to store info and read_set to indicate socketsready to read
+  fd_set master_set, read_set;
+  socklen_t cli_size;
+  struct sockaddr_in addr, cli_addr;
+  char buf[BUF_SIZE];
+  int i;
+  ssize_t nbytes;
 
-    fprintf(stdout, "----- Echo Server -----\n");
+  fprintf(stdout, "----- Echo Server -----\n");
 
-    /* all networked programs must create a socket */
-    if ((server_sock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
-        fprintf(stderr, "Failed creating socket.\n");
-        return EXIT_FAILURE;
+  /* all networked programs must create a socket */
+  if ((server_sock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+    fprintf(stderr, "Failed creating socket.\n");
+    return EXIT_FAILURE;
+  }
+  fprintf(stdout, "server socket created: %d.\n", server_sock);
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(ECHO_PORT);
+  addr.sin_addr.s_addr = INADDR_ANY;
+
+  /* servers bind sockets to ports---notify the OS they accept connections */
+  // configure socket for reuse (http://stackoverflow.com/questions/14388706/socket-options-so-reuseaddr-and-so-reuseport-how-do-they-differ-do-they-mean-t)
+  if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0) {
+    close_socket(server_sock);
+    fprintf(stderr, "setsockopt(SO_REUSEADDR) failed.\n");
+    return EXIT_FAILURE;
+  }
+
+  if (bind(server_sock, (struct sockaddr *) &addr, sizeof(addr))) {
+    close_socket(server_sock);
+    fprintf(stderr, "Failed binding socket.\n");
+    return EXIT_FAILURE;
+  }
+  fprintf(stdout, "server socket bound.\n");
+
+  if (listen(server_sock, 5)) {
+    close_socket(server_sock);
+    fprintf(stderr, "Error listening on socket.\n");
+    return EXIT_FAILURE;
+  }
+  fprintf(stdout, "server socket listening.\n");
+
+  FD_ZERO(&master_set);
+  FD_ZERO(&read_set);
+  FD_SET(server_sock, &master_set);
+  min_sock = server_sock;
+  max_sock = server_sock;
+
+  for(;;) {
+    read_set = master_set;
+    fprintf(stdout, "waiting for event on select.\n");
+    if (select(max_sock + 1, &read_set, NULL, NULL, NULL) == -1) { //block until we have an event
+      fprintf(stderr, "Error exiting select.\n");
+      return EXIT_FAILURE;
     }
-    fprintf(stdout, "server socket created: %d.\n", server_sock);
 
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(ECHO_PORT);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    for (i = min_sock; i <= max_sock; i++) {
+      fprintf(stdout, "select event. checking socket: %d.\n", i);
 
-    /* servers bind sockets to ports---notify the OS they accept connections */
-    if (bind(server_sock, (struct sockaddr *) &addr, sizeof(addr))) {
-        close_socket(server_sock);
-        fprintf(stderr, "Failed binding socket.\n");
-        return EXIT_FAILURE;
-    }
-    fprintf(stdout, "server socket bound.\n");
-
-    if (listen(server_sock, 5)) {
-        close_socket(server_sock);
-        fprintf(stderr, "Error listening on socket.\n");
-        return EXIT_FAILURE;
-    }
-    fprintf(stdout, "server socket listening.\n");
-
-    FD_ZERO(&master_set);
-    FD_ZERO(&read_set);
-    FD_SET(server_sock, &master_set);
-    min_sock = server_sock;
-    max_sock = server_sock;
-
-    for(;;) {
-      read_set = master_set;
-      if (select(max_sock + 1, &read_set, NULL, NULL, NULL) == -1) { //block until we have an event
-        fprintf(stderr, "Error exiting select.\n");
-        return EXIT_FAILURE;
-      }
-
-      for (i = min_sock; i <= max_sock; i++) {
-        fprintf(stdout, "select event. checking socket: %d.\n", i);
-
-        //check for connection requests
+      if (FD_ISSET(i, &read_set)) { // check for events on the socket fd
         if (i == server_sock) { // we have a connection request
           fprintf(stdout, "connection request.\n");
           cli_size = sizeof(cli_addr);
@@ -88,6 +96,7 @@ int main(int argc, char* argv[]) {
             fprintf(stderr, "Error accepting connection.\n");
             return EXIT_FAILURE;
           } else {
+            fprintf(stdout, "connection accepted on socket: %d", new_sock);
             FD_SET(new_sock, &master_set);
             if(new_sock > max_sock) {
               max_sock = new_sock;
@@ -111,62 +120,64 @@ int main(int argc, char* argv[]) {
               close_socket(server_sock);
               fprintf(stderr, "Error sending to client.\n");
               return EXIT_FAILURE;
-             }
-             memset(buf, 0, BUF_SIZE);
+            }
+            fprintf(stdout, "sent data to client: %d\n", i);
+            memset(buf, 0, BUF_SIZE);
           }
         }
-      }
-    }
+      } // check if socket file descriptor is in the set from select (has an event)
+    } // iterating over socket file descriptors
+  } // infinite for loop
 
 
-    close_socket(server_sock);
-    return EXIT_SUCCESS;
+  close_socket(server_sock);
+  return EXIT_SUCCESS;
 
 
 
 
-    // /* finally, loop waiting for input and then write it back */
-    // while (1) {
-    //    cli_size = sizeof(cli_addr);
-    //    if ((client_sock = accept(server_sock, (struct sockaddr *) &cli_addr, &cli_size)) == -1) {
-    //        close(server_sock);
-    //        fprintf(stderr, "Error accepting connection.\n");
-    //        return EXIT_FAILURE;
-    //    }
-    //
-    //    readret = 0;
-    //    while((readret = recv(client_sock, buf, BUF_SIZE, 0)) >= 1) {
-    //        if (send(client_sock, buf, readret, 0) != readret) {
-    //            close_socket(client_sock);
-    //            close_socket(server_sock);
-    //            fprintf(stderr, "Error sending to client.\n");
-    //            return EXIT_FAILURE;
-    //        }
-    //        memset(buf, 0, BUF_SIZE);
-    //    }
-    //
-    //    if (readret == -1) {
-    //        close_socket(client_sock);
-    //        close_socket(server_sock);
-    //        fprintf(stderr, "Error reading from client socket.\n");
-    //        return EXIT_FAILURE;
-    //    }
-    //
-    //    if (close_socket(client_sock)) {
-    //        close_socket(server_sock);
-    //        fprintf(stderr, "Error closing client socket.\n");
-    //        return EXIT_FAILURE;
-    //    }
-    // }
-    //
-    // close_socket(server_sock);
-    // return EXIT_SUCCESS;
+  // /* finally, loop waiting for input and then write it back */
+  // while (1) {
+  //    cli_size = sizeof(cli_addr);
+  //    if ((client_sock = accept(server_sock, (struct sockaddr *) &cli_addr, &cli_size)) == -1) {
+  //        close(server_sock);
+  //        fprintf(stderr, "Error accepting connection.\n");
+  //        return EXIT_FAILURE;
+  //    }
+  //
+  //    readret = 0;
+  //    while((readret = recv(client_sock, buf, BUF_SIZE, 0)) >= 1) {
+  //        if (send(client_sock, buf, readret, 0) != readret) {
+  //            close_socket(client_sock);
+  //            close_socket(server_sock);
+  //            fprintf(stderr, "Error sending to client.\n");
+  //            return EXIT_FAILURE;
+  //        }
+  //        memset(buf, 0, BUF_SIZE);
+  //    }
+  //
+  //    if (readret == -1) {
+  //        close_socket(client_sock);
+  //        close_socket(server_sock);
+  //        fprintf(stderr, "Error reading from client socket.\n");
+  //        return EXIT_FAILURE;
+  //    }
+  //
+  //    if (close_socket(client_sock)) {
+  //        close_socket(server_sock);
+  //        fprintf(stderr, "Error closing client socket.\n");
+  //        return EXIT_FAILURE;
+  //    }
+  // }
+  //
+  // close_socket(server_sock);
+  // return EXIT_SUCCESS;
 }
 
 int close_socket(int sock) {
-    if (close(sock)) {
-        fprintf(stderr, "Failed closing socket.\n");
-        return 1;
-    }
-    return 0;
+  if (close(sock)) {
+    fprintf(stderr, "Failed closing socket.\n");
+    return 1;
+  }
+  return 0;
 }
