b3bdab6c9537e8d599d51ee2e1a011c3b4aacca3
trvslhlt <trvslhlt@gmail.com>
2017-02-27 01:05:08 -0500

    code formatting

--- http.c
+++ http.c
@@ -11,45 +11,43 @@
 typedef enum { false, true } bool;
 
 // helper functions for assembling strings
-char * get_reason(int);
-char * assemble_status_line(int);
-char * get_response(Request *, char *, bool);
-char * make_header(char *, char *);
-char * get_header_value(Request *, char *);
-char * get_mime_type(char *);
+char* get_reason(int);
+char* assemble_status_line(int);
+char* get_response(Request *, char *, bool);
+char* make_header(char *, char *);
+char* get_header_value(Request *, char *);
+char* get_mime_type(char *);
 
 // this function parses the request in request_buf and creates a response, which it writes to response
-void http_parse_request(char * request_buf, int size, int socketFd, char * response) {
-  Request * request = parse(request_buf, size, socketFd);
-
-  char * method = request->http_method;
-
-  char * header = malloc(4096); // **** ADDRESS THIS
-  char * entity_buffer = malloc(10000); // AND THIS
+void http_parse_request(char *request_buf, int size, int socketFd, char *response) {
+  Request *request = parse(request_buf, size, socketFd);
+  char *method = request->http_method;
+  char *header = malloc(4096); // **** ADDRESS THIS
+  char *entity_buffer = malloc(10000); // AND THIS
 
   if (strcmp(method,"GET") == 0) {
     printf("GET request made\n");
     header = get_response(request, entity_buffer, true);
-
     strcpy(response, header); // start with the headers
     strcat(response, entity_buffer); // add the actual content of the response
     strcat(response, "\r\n"); // to end response
   }
+  
   if (strcmp(method,"POST") == 0) {
     printf("POST request made\n");
   }
+  
   if (strcmp(method,"HEAD") == 0) {
     printf("HEAD request made\n");
     header = get_response(request, entity_buffer, false);
     strcpy(response, header);
   }
-  // **** need to handle other methods (by generating 501)
+  //TODO: handle other methods (by generating 501)
 }
 
-
 // helper function to get reason strings
-char * get_reason(int status_code) {
-  char * reason;
+char* get_reason(int status_code) {
+  char *reason;
   switch (status_code) {
     case 200:
       reason = "OK";
@@ -72,6 +70,8 @@ char * get_reason(int status_code) {
     case 505:
       reason = "HTTP Version not supported";
       break;
+    default:
+      reason = "Internal Server Error";
   }
 
   return reason;
@@ -79,10 +79,10 @@ char * get_reason(int status_code) {
 
 // this function returns the headers, and if the request is a GET,
 // it will also write the file content to entity_buffer (for head, will skip this step)
-char * get_response(Request * request, char * entity_buffer, bool head_only) {
-  char * header = malloc(4096);
-  char * statusline = malloc(4096);
-  FILE * file_requested;
+char* get_response(Request *request, char *entity_buffer, bool head_only) {
+  char *header = malloc(4096);
+  char *statusline = malloc(4096);
+  FILE *file_requested;
 
   // time formatting code, from http://stackoverflow.com/questions/7548759/generate-a-date-string-in-http-response-date-format-in-c
   char timebuf[1000];
@@ -94,12 +94,10 @@ char * get_response(Request * request, char * entity_buffer, bool head_only) {
   if(strcmp(request->http_version, "HTTP/1.1") != 0) { // throw 505 if client is using diff HTTP version
     statusline = assemble_status_line(505);
     strcpy(header, statusline);
-  }
-  else if (access(request->http_uri + 1, F_OK) == -1) { // throw 404 if file not accessible
+  } else if (access(request->http_uri + 1, F_OK) == -1) { // throw 404 if file not accessible
     statusline = assemble_status_line(404);
     strcpy(header, statusline);
-  }
-  else {
+  } else {
     file_requested = fopen(request->http_uri + 1, "r"); // open the file requested
 
     // get file size
@@ -110,10 +108,10 @@ char * get_response(Request * request, char * entity_buffer, bool head_only) {
     char size_str[200];
     sprintf(size_str, "%d", file_size); // convert file size to a string
 
-    char * mime = malloc(30);
+    char *mime = malloc(30);
 
     // get the file extension
-    char * ext = strrchr(request->http_uri + 1, '.');
+    char *ext = strrchr(request->http_uri + 1, '.');
 
     // determine MIME type
     if (!ext) { // null extension
@@ -161,11 +159,11 @@ char * get_response(Request * request, char * entity_buffer, bool head_only) {
   return header;
 }
 
-char * assemble_status_line(int status_code) {
-  char * status_line = malloc(500); // ********* may be overkill
-  char * http_version = "HTTP/1.1";
+char* assemble_status_line(int status_code) {
+  char *status_line = malloc(500); // ********* may be overkill
+  char *http_version = "HTTP/1.1";
 
-  char * reason = get_reason(status_code);
+  char *reason = get_reason(status_code);
 
   char status_str[3];
   sprintf(status_str, "%d", status_code);
@@ -181,32 +179,27 @@ char * assemble_status_line(int status_code) {
 }
 
 // helper function to get mime types based on extensions
-char * get_mime_type(char * extension) {
-  char * mime_type = malloc(50);
+char* get_mime_type(char *extension) {
+  char *mime_type = malloc(50);
 
   if(strcmp(extension, "html") == 0) {
     strcpy(mime_type, "text/html");
-  }
-  else if(strcmp(extension, "css") == 0) {
+  } else if(strcmp(extension, "css") == 0) {
     strcpy(mime_type, "text/css");
-  }
-  else if(strcmp(extension, "png") == 0) {
+  } else if(strcmp(extension, "png") == 0) {
     strcpy(mime_type, "image/png");
-  }
-  else if(strcmp(extension, "jpeg") == 0) {
+  } else if(strcmp(extension, "jpeg") == 0) {
     strcpy(mime_type, "image/jpeg");
-  }
-  else if(strcmp(extension, "gif") == 0) {
+  } else if(strcmp(extension, "gif") == 0) {
     strcpy(mime_type, "image/gif");
-  }
-  else {
+  } else {
     strcpy(mime_type, "unknown");
   }
 
   return mime_type;
 }
 
-char * get_header_value(Request * request, char * name) {
+char* get_header_value(Request * request, char *name) {
     int index = 0;
 
     for(index = 0; index < request->header_count; index++) {
@@ -214,15 +207,14 @@ char * get_header_value(Request * request, char * name) {
         return request->headers[index].header_value;
       }
     }
-
-    // if header not set, return special string
-    char * nullstr = "NULL";
+    
+    char *nullstr = "NULL";
     return nullstr;
 }
 
 // helper to create a new header field
-char * make_header(char * name, char * value) {
-  char * header = malloc(2096);
+char* make_header(char *name, char *value) {
+  char *header = malloc(2096);
 
   strcpy(header, name);
   strcat(header, ": ");
