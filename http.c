551a099648322dcf70eb9e8e5dc113ec0914ee05
trvslhlt <trvslhlt@gmail.com>
2017-03-01 03:04:58 -0500

    more semantic code.
    - working on the second request bug that came back.
    - making the mime type work --- looks like the uri going into does not
      match the request

--- http.c
+++ http.c
@@ -1,7 +1,5 @@
 #include <time.h>
 #include <unistd.h>
-
-// imports for parse
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -10,17 +8,17 @@
 #include "marshal.h"
 
 #define BIG_DUMB_NUMBER 10000
-
 typedef enum { false, true } bool;
 enum { GET = 0, HEAD, POST };
 
-// helper functions for assembling strings
 Response get_default_response(int);
 Response get_custom_response(Request *, int);
+Response_header_field get_header_field(char *, char *);
+Response_header_field get_date_header_field();
+Response_header_field get_modified_time_header_field(char *filepath);
 int handle_request(Request *request, Response *response);
 char* get_reason(int);
 char* assemble_status_line(int);
-char* make_header(char *, char *);
 char* get_mime_type(char *);
 int set_serve_folder(char *);
 int set_continue(bool);
@@ -99,24 +97,7 @@ int handle_request(Request *request, Response *response) {
     *response = get_default_response(501);
     return 0;
   }
-
   *response = get_custom_response(request, method);
-
-  // TODO: use below code for anything?
-  // switch (method) {
-  //   case GET:
-  //     *response = get_default_response(500);
-  //     break;
-  //   case HEAD:
-  //     *response = get_default_response(500);
-  //     break;
-  //   case POST:
-  //     *response = get_default_response(500);
-  //     break;
-  //   default:
-  //     *response = get_default_response(501);
-  // }
-
   return 0;
 }
 
@@ -148,36 +129,6 @@ char* assemble_status_line(int status_code) {
   return status_line;
 }
 
-// helper function to get mime types based on extensions
-char* get_mime_type(char *extension) {
-  char *mime_type = malloc(50);
-
-  if(strcmp(extension, "html") == 0) {
-    strcpy(mime_type, "text/html");
-  } else if(strcmp(extension, "css") == 0) {
-    strcpy(mime_type, "text/css");
-  } else if(strcmp(extension, "png") == 0) {
-    strcpy(mime_type, "image/png");
-  } else if(strcmp(extension, "jpeg") == 0) {
-    strcpy(mime_type, "image/jpeg");
-  } else if(strcmp(extension, "gif") == 0) {
-    strcpy(mime_type, "image/gif");
-  } else {
-    strcpy(mime_type, "unknown");
-  }
-  return mime_type;
-}
-
-// helper to create a new header field
-char* make_header(char *name, char *value) {
-  char *header = malloc(2096);
-  strcpy(header, name);
-  strcat(header, ": ");
-  strcat(header, value);
-  strcat(header, "\r\n");
-  return header;
-}
-
 Response get_default_response(int status_code) {
   Response_header header;
   strcpy(header.http_version, "HTTP/1.1");
@@ -191,53 +142,63 @@ Response get_default_response(int status_code) {
   return (Response){header, NULL};
 }
 
-Response get_custom_response(Request *request, int method) {
-  char *entity_buffer = malloc(10000);
-  Response_header header;
-  int field_count = 0;
-  FILE *file_requested;
-
+Response_header_field get_date_header_field() {
   // time formatting code, from http://stackoverflow.com/questions/7548759/generate-a-date-string-in-http-response-date-format-in-c
   char timebuf[300];
   time_t now = time(0);
   struct tm tm = *gmtime(&now);
   strftime(timebuf, sizeof(timebuf), "%a, %d %b %Y %H:%M:%S %Z", &tm);
+  return get_header_field("Date", timebuf);
+}
+
+Response_header_field get_modified_time_header_field(char *filepath) {
+  struct stat attr;
+  stat(filepath, &attr);
+  char modtimebuf[200];
+  time_t modified_time = attr.st_mtime;
+  struct tm tmx = *gmtime(&modified_time);
+  strftime(modtimebuf, sizeof(modtimebuf), "%a, %d %b %Y %H:%M:%S %Z", &tmx);
+  return get_header_field("Modified-Time", modtimebuf);
+}
+
+Response get_custom_response(Request *request, int method) {
+  Response response;
+  response.body = NULL;
+  int field_count = 0;
+  FILE *file_requested;
 
   char *filepath = malloc(300);
   strcpy(filepath, permanent_serve_folder);
   strcat(filepath, "/");
   strcat(filepath, request->header.uri + 1);
 
-  strcpy(header.http_version, "HTTP/1.1");
 
+  strcpy(response.header.http_version, "HTTP/1.1");
+  strcpy(response.header.http_version, "HTTP/1.1");
+  
   // default fields
-  Response_header_field server_field;
-  strcpy(server_field.name, "Server");
-  strcpy(server_field.value, "Liso/1.0");
+  Response_header_field server_field = get_header_field("Server", "Liso/1.0");
   field_count++;
-
-  Response_header_field date_field;
-  strcpy(date_field.name, "Date");
-  strcpy(date_field.value, timebuf);
+  Response_header_field date_field = get_date_header_field();
   field_count++;
 
   // status line creation
   if(strcmp(request->header.http_version, "HTTP/1.1") != 0) { // throw 505 if client is using diff HTTP version
-    header.status_code = 505;
+    response.header.status_code = 505;
     Response_header_field connection_field;
     strcpy(connection_field.name, "Connection");
     strcpy(connection_field.value, "close");
     field_count++;
     Response_header_field fields[3] = {server_field, date_field, connection_field};
-    header.fields = fields;
-    header.field_count = field_count;
-    return (Response){header, NULL};
+    response.header.fields = fields;
+    response.header.field_count = field_count;
+    return response;
   } else if (access(filepath, F_OK) == -1) { // throw 404 if file not accessible
-    header.status_code = 505;
+    response.header.status_code = 505;
     Response_header_field fields[2] = {server_field, date_field};
-    header.fields = fields;
-    header.field_count = field_count;
-    return (Response){header, NULL};
+    response.header.fields = fields;
+    response.header.field_count = field_count;
+    return response;
   } else {
     file_requested = fopen(filepath, "r"); // open the file requested
 
@@ -247,77 +208,51 @@ Response get_custom_response(Request *request, int method) {
     fseek(file_requested, 0L, SEEK_SET);
     char *size_str = malloc(200);
     sprintf(size_str, "%d", file_size); // convert file size to a string
-    char *mime = malloc(30);
-    char *ext = strrchr(request->header.uri + 1, '.'); // get the file extension
+    char *mime = get_mime_type(request->header.uri);
 
-    if (!ext) { // null extension
-        strcpy(mime, "unknown");
-    } else { // get mime type from helper function
-        strcpy(mime, get_mime_type(ext+1));
-    }
-
-    // if we're getting more than head, copy the file into our entity buffer
+    char *entity_buffer = malloc(file_size);
     if(method == GET) {
-      fread(entity_buffer, file_size, 1, file_requested); // WARNING: entity_buffer is allocated 10000 bytes before it is passed in,
-                                                          // so this copy is unsafe if the file is larger than this
+      fread(entity_buffer, file_size, 1, file_requested);
+      response.body = entity_buffer;
     }
-
     fclose(file_requested);
 
-    // get modified time
-    struct stat attr;
-    stat(filepath, &attr);
-
-    // save last modified time to string
-    char modtimebuf[200];
-    time_t modified_time = attr.st_mtime;
-    struct tm tmx = *gmtime(&modified_time);
-    strftime(modtimebuf, sizeof(modtimebuf), "%a, %d %b %Y %H:%M:%S %Z", &tmx);
-
     if((method == POST) && (following_continue == false)) { // send a continue
-      header.status_code = 100;
+      response.header.status_code = 100;
       last_post_req = request;
     } else {
-      header.status_code = 200;
+      response.header.status_code = 200;
     }
 
     // assemble the headers
     if((method == GET) || (method == HEAD)) { // no need if the content is a post
-      Response_header_field content_length_field;
-      strcpy(content_length_field.name, "Content-Length");
-      strcpy(content_length_field.value, size_str);
+      Response_header_field content_length_field = get_header_field("Content-Length", size_str);
+      field_count++;
+      Response_header_field content_type_field = get_header_field("Content-Type", mime);
+      field_count++;
+      Response_header_field mod_time_field = get_modified_time_header_field(filepath);
       field_count++;
 
-      Response_header_field content_type_field;
-      strcpy(content_type_field.name, "Content-Type");
-      strcpy(content_type_field.value, mime);
-      field_count++;
-
-      Response_header_field mod_time_field;
-      strcpy(mod_time_field.name, "Modified-Time");
-      strcpy(mod_time_field.value, modtimebuf);
-      field_count++;
-
-      Response_header_field fields[5] = {content_length_field, content_type_field, mod_time_field, server_field, date_field};
-      header.fields = fields;
-      header.field_count = field_count;
-
-      if(method == GET) {
-        return (Response){header, entity_buffer};
-      } else {
-        return (Response){header, NULL};
-      }
+      Response_header_field fields[5] = {
+        content_length_field, 
+        content_type_field, 
+        mod_time_field, 
+        server_field, 
+        date_field};
+      response.header.fields = fields;
+      response.header.field_count = field_count;
+      return response;
     } else {
-      Response_header_field content_length_field;
-      strcpy(content_length_field.name, "Content-Length");
-      strcpy(content_length_field.value, "0");
+      Response_header_field content_length_field = get_header_field("Content-Length", "0");
       field_count++;
 
-      Response_header_field fields[3] = {content_length_field, server_field, date_field};
-      header.fields = fields;
-      header.field_count = field_count;
-
-      return (Response){header, NULL};
+      Response_header_field fields[3] = {
+        content_length_field, 
+        server_field, 
+        date_field};
+      response.header.fields = fields;
+      response.header.field_count = field_count;
+      return response;
     }
   }
 }
@@ -331,4 +266,40 @@ int set_serve_folder(char *serve_folder) {
 int set_continue(bool continue_status) {
   following_continue = continue_status;
   return 0;
+}
+
+// helper function to get mime types based on extensions
+char* get_mime_type(char *uri) {
+  printf("--- get_mime_type uri: %s\n", uri);
+  char *extension = strrchr(uri + 1, '.'); // get the file extension
+  if (!extension) { // null extension
+    printf("--- get_mime_type unknown\n");
+    return "unknown";
+  } 
+  printf("--- extension %s\n", extension);
+  extension += 1; // ???? taken from Charlie's -> strcpy(mime, get_mime_type(ext+1));
+  printf("--- extension+1 %s\n", extension);
+  printf("--- get_mime_type unknown\n");
+  if(strcmp(extension, "html") == 0) {
+    return "text/html";
+  } else if(strcmp(extension, "css") == 0) {
+    return "text/css";
+  } else if(strcmp(extension, "png") == 0) {
+    return "image/png";
+  } else if(strcmp(extension, "jpeg") == 0) {
+    return "image/jpeg";
+  } else if(strcmp(extension, "gif") == 0) {
+    return "image/gif";
+  } else {
+    return "unknown";
+  }
+}
+
+// helper to create a new header field
+
+Response_header_field get_header_field(char *name, char *value) {
+  Response_header_field field;
+  strcpy(field.name, name);
+  strcpy(field.value, value);
+  return field;
 }
